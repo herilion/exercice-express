102386be6b353af16caf4349ee85ebcaa33233db
herilion <heritierlionge@yahoo.com>
2022-10-19 16:01:32 +0200

    update

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/pages/page1.html')
+});
+
+app.get("/page2", (req, res) => {
+    res.sendFile(__dirname + '/pages/page2.html')
+});
+
+app.get("/pages3", (req, res) => {
+    res.sendFile(__dirname + '/pages/page3.html')
 })
 app.listen(8000)
