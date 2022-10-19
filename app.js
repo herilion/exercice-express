c3f3868deb922f790196d0149074479c36d0019e
herilion <heritierlionge@yahoo.com>
2022-10-19 15:54:29 +0200

    appel page1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-    res.send('Bonjour la salle, il est ' + dateNow)
+    res.sendFile(__dirname + '/pages/page1.html')
 })
 app.listen(8000)
