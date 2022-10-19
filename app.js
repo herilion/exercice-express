b884d0727fdaee79f1b87552ed420014d5525c22
herilion <heritierlionge@yahoo.com>
2022-10-19 16:12:02 +0200

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/page2", (req, res) => {
     res.sendFile(__dirname + '/pages/page2.html')
 });
 
-app.get("/pages3", (req, res) => {
+app.get("/page3", (req, res) => {
     res.sendFile(__dirname + '/pages/page3.html')
 })
 app.listen(8000)
